6a8b1439a67690bfd5525da40102792ab5837fd6
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-11 15:36:02 -0400

    edited schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+
+CREAT TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
+);
