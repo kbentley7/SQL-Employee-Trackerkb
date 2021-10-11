f5a396731b1413d273eb1fd6ce717b61147455f3
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-11 13:42:00 -0400

    edited server.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const mysql = require ('mysq12');
 const inquirer = require ('inquirer');
 const cTable = require ('console.table');
+
 require('dotenv').config()
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.MYSQL_PASSWORD,
     datbase: 'employee_db'
+});
 
-})
+connection.connect(er => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+});
+
+afterConnection = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    promptUser();
+  };
