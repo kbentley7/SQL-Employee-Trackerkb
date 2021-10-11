29e1288fd03075c8087eabf342d419901e41ae9b
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-11 16:55:53 -0400

    edited seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,14 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Full Stack Developer', 80000, 1),
-('Software Engineer', 120000, 1),
-('Accountant', 10000, 2), 
-('Finanical Analyst', 150000, 2),
-('Marketing Coordindator', 70000, 3), 
-('Sales Lead', 90000, 3),
-('Project Manager', 100000, 4),
-('Operations Manager', 90000, 4);
+('Full Stack Developer', 70000, 1),
+('Software Engineer', 110000, 1),
+('Accountant', 9000, 2), 
+('Finanical Analyst', 140000, 2),
+('Marketing Coordindator', 60000, 3), 
+('Sales Lead', 80000, 3),
+('Project Manager', 90000, 4),
+('Operations Manager', 80000, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
