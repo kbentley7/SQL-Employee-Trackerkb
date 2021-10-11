49cf9a32f9bcecf1c95c7c78a78fd3618c4b81f8
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-11 15:21:46 -0400

    edited server.js

--- server.js
+++ server.js
@@ -47,3 +47,64 @@ afterConnection = () => {
                   'No Action']
       }
     ]) 
+
+    .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all roles") {
+          showRoles();
+        }
+  
+        if (choices === "View all employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add a role") {
+          addRole();
+        }
+  
+        if (choices === "Add an employee") {
+          addEmployee();
+        }
+  
+        if (choices === "Update an employee role") {
+          updateEmployee();
+        }
+  
+        if (choices === "Update an employee manager") {
+          updateManager();
+        }
+  
+        if (choices === "View employees by department") {
+          employeeDepartment();
+        }
+  
+        if (choices === "Delete a department") {
+          deleteDepartment();
+        }
+  
+        if (choices === "Delete a role") {
+          deleteRole();
+        }
+  
+        if (choices === "Delete an employee") {
+          deleteEmployee();
+        }
+  
+        if (choices === "View department budgets") {
+          viewBudget();
+        }
+  
+        if (choices === "No Action") {
+          connection.end()
+      };
+    });
+  };
