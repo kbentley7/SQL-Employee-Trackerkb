0303be454fa5852b345430add56bf2a1e5e32838
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-25 19:47:40 -0400

    minor edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,40 +1,33 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Bruce', 'Lee', 1, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jim', 'Kelley', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jet', 'Li', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Sonny', 'Chiba', 4, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Angela', 'Mao', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Gordon', 'Lui', 5, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Polly', 'Shang-Kuan', 6, null);
 
+USE employee_db;
 
-INSERT INTO department (department_name)
-VALUES ('Management');
-INSERT INTO department (department_name)
-VALUES ('Sales');
-INSERT INTO department (department_name)
-VALUES ('Accounting');
-INSERT INTO department (department_name)
-VALUES ('Reception');
-INSERT INTO department (department_name)
-VALUES ('Human Resources');
+--Seed data from Patrick B--
 
+--add department
+INSERT INTO department
+    (name)
+VALUES
+    ("Human Resources"),
+    ("R&D"),
+    ("Engineering"),
+    ("Accounting"),
+    ("Sales");
 
-NSERT INTO role (title, salary, department_id)
-VALUES ('General Manager', 120000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Salesman', 80000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Accountant', 90000, 4);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Receptionist', 40000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ('Human Resource Officer', 75000, 5);
-INSERT INTO role (title, salary, department_id)
-VALUES ('CEO', 250000, null);
+-- add role
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+("manager", 75000.00, 2),
+("engineer", 52000, 3),
+("accountant", 62500, 4),
+("recruiter", 57500, 1),
+("sales person", 85650, 5);
+
+--add employee
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+("Bruce", "Lee", 1, NULL),
+("Angela", "Mao", 1, 1),
+("Jet", "Li", 3, 2),
+("Sonny", "Chiba", 5, 2);
